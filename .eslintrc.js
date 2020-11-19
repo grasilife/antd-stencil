e52f92649a7f5734a7f95bdc042f3d9ab2654c4d
fuguoqiang1 <fuguoqiang1@100tal.com>
2020-11-19 14:27:18 +0800

    feat(others): 添加eslint和prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'react', 'prettier'],
+  extends: ['standard', 'prettier', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+  rules: {
+    '@typescript-eslint/no-use-before-define': 0,
+    'no-use-before-define': [0],
+    'react/prop-types': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  env: {
+    'jest/globals': true,
+    browser: true,
+    node: true,
+    es6: true,
+  },
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+}
