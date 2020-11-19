16d92980d1b65ad34ba05c5147b9b75274898732
fuguoqiang1 <fuguoqiang1@100tal.com>
2020-11-19 23:23:50 +0800

    chore(others): 添加puppeteer测试

--- packages/antd-stencil-component/src/components/my-component/my-component.e2e.ts
+++ packages/antd-stencil-component/src/components/my-component/my-component.e2e.ts
@@ -15,18 +15,18 @@ describe('my-component', () => {
     await page.setContent('<my-component></my-component>');
     const component = await page.find('my-component');
     const element = await page.find('my-component >>> div');
-    expect(element.textContent).toEqual(`Hello, World! I'm `);
+    expect(element.textContent).toEqual(`Hello, World! Im `);
 
     component.setProperty('first', 'James');
     await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James`);
+    expect(element.textContent).toEqual(`Hello, World! Im James`);
 
     component.setProperty('last', 'Quincy');
     await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Quincy`);
+    expect(element.textContent).toEqual(`Hello, World! Im James Quincy`);
 
     component.setProperty('middle', 'Earl');
     await page.waitForChanges();
-    expect(element.textContent).toEqual(`Hello, World! I'm James Earl Quincy`);
+    expect(element.textContent).toEqual(`Hello, World! Im James Earl Quincy`);
   });
 });
